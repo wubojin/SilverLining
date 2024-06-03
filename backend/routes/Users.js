abdcea08aa640849c80a10f97ae9bf70a795c0c9
wubojin <wubojin64@gmail.com>
2024-06-03 11:30:03 +0800

    Initial commit

---
+++ backend/routes/Users.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const {Users} = require("../models");
+const bcrypt = require("bcrypt");
+
+const {} = require("jsonwebtoken");
+
+router.post("/signup", async (req, res) => {
+    const {username, password} = req.body;
+    bcrypt.hash(password, 10).then((hash) => {
+        Users.create({
+            username: username,
+            password: hash,
+        });
+        res.json("SUCCESS!");
+    });
+});
+
+router.post("/login", async (req, res) => {
+    const {username, password} = req.body;
+    const user = await Users.findOne({ where: {username: username} });
+
+    if (!user) res.json({error: "User doesn't exist!"});
+
+    bcrypt.compare(password, user.password).then((match) => {
+        if (!match) res.json({error: "Wrong Username and Password combination"});
+
+        const accessToken = sign({username: user.username, id: user.id}, "importantsecret");
+        res.json(accessToken);
+    });
+});
+
+module.exports = router;
