ce21456c1f6570e27bd934afbfc9c5f186540774
wubojin <wubojin64@gmail.com>
2024-07-05 22:34:39 +0800

    14th commit

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
