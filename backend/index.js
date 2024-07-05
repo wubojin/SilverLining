0c4410b571957c27d5ceb2f4fdee069e83e0a982
wubojin <wubojin64@gmail.com>
2024-07-05 22:04:40 +0800

    11th commit

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
+
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 const db = require("./models");
 
@@ -20,6 +25,6 @@ app.use("/applications", applicationsRouter);
 const studygroupsRouter = require("./routes/StudyGroups");
 app.use("/studygroups", studygroupsRouter);
 
-app.listen(process.env.PORT || PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
