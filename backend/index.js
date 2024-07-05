53e49f3133afc0af94c1e1561104706fae10c4a2
wubojin <wubojin64@gmail.com>
2024-07-05 22:16:23 +0800

    12th commit

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "frontend")));
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
 const db = require("./models");
